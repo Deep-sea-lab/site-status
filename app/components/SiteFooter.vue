13b2651ad1e3a293e64994b2271d970e98ba0195
l cx <3800907852@qq.com>
2025-04-13 18:05:17 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -51,7 +51,7 @@ const { siteIcp, version } = configPublic;
 
 const linkData = {
   github: "https://github.com/deep-sea-lab",
-  home: "https://www.deep-sea.us.kg",
+  home: "https://www.deep-sea.dpdns.org",
   email: "mailto:lcx1029lcx1029@dingtalk.com",
 };
 </script>
